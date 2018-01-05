13156d0d14ae3a5aff2c733d4d5e20584a800214
atusy <dummy@dummy.dummy>
2018-01-05 20:30:42 +0900

    Become more robust by excluding mapping pixels who are analyzing mixture phases based on prediction interval of lm(map ~ 0 + pkint) In addition, when all points of certain phase are excluded, expected counts in mapping are estimated based on the above model

--- R/qltmap_cls_centers.R
+++ R/qltmap_cls_centers.R
@@ -7,84 +7,66 @@
 #' @param cnd_qltmap path to the condition file of mapping data
 #' @param saving file name to save. FALSE if not saving.
 #'
-#' @importFrom data.table as.data.table
-#' @importFrom data.table fread
 #' @importFrom data.table fwrite
-#' @importFrom data.table setcolorder
-#' @importFrom dplyr arrange
 #' @importFrom dplyr filter
 #' @importFrom dplyr group_by
-#' @importFrom dplyr summarise_all
+#' @importFrom dplyr summarise
 #' @importFrom dplyr mutate
-#' @importFrom dplyr mutate_all
-#' @importFrom dplyr distinct
 #' @importFrom pipeR %>>%
-#' @importFrom stringr str_replace
-#' @importFrom stringr str_replace_all
-#' @importFrom stringr str_detect
-#' @importFrom tidyr gather
 #' @importFrom tidyr spread
 #'
 #' @export
-qltmap_cls_centers <- function(qnt = NULL, qltmap = NULL, wd = NULL, dir_map = NULL, cnd_qltmap = '0.cnd', saving = 'centers_initial0.csv') {
+qltmap_cls_centers <- function(
+  wd = NULL,
+  dir_map,
+  phase_list = NULL,
+  phase_fine = NULL,
+  qnt = qnt_load(wd, phase_list = phase_list),
+  qltmap = qltmap_load(dir_map),
+  saving = 'centers_initial0.csv'
+) {
 
-  cd <- getwd()
-  on.exit(setwd(cd))
-  if(!is.null(wd)) setwd(wd)
-
-  #qnt: import quantitative data
-  if(is.null(qnt)) qnt <- qnt_load('../../')
-  if(is.character(qnt)) qnt <- readRDS(qnt)
-  if(!all(class(qnt) == c('list', 'qnt'))) stop('illegal input of qnt')
-
-  #qltmap: import mapping data
-  if(is.null(qltmap)) qnt <- qltmap_load(dir_map)
-  if(is.character(qltmap)) qltmap <- readRDS(qltmap)
-  if(!all(class(qltmap) == c('list', 'qltmap'))) stop('illegal input of qltmap')
-
-  #cnd: import analysis conditions from 0.cnd
-  cnd <- dir_map %>>%
-    paste(cnd_qltmap, sep = '/') %>>%
-    readLines %>>%
-    '['(str_detect(.,
-      '(Measurement Start Position X)|(Measurement Start Position Y)|(X-axis Step Number)|(Y-axis Step Number)|(X-axis Step Size)|(Y-axis Step Size)|(X Step Size)|(Y Step Size)'
-      )) %>>%
-    str_replace_all('[:blank:].*', '') %>>%
-    as.numeric %>>%
-    matrix(ncol = 3, nrow = 2, dimnames = list(NULL, c('start', 'px', 'step'))) %>>%
-    as.data.table
-
-  #qnt =================================================================
-  #qnt: convert stage coordinates to map coordinates
-  #qnt: and then remove points with coordinates out of maps
-  qnt$cnd <- qnt$cnd %>>%
-  #  mutate(x.stage = x, y.stage = y) %>>%
+  epma_tidy( # Compile spot/map analysis
+    wd,
+    dir_map,
+    phase_list,
+    qnt = qnt,
+    qltmap = qltmap,
+    cluster = NA
+  ) %>>%
+    mutate( # Let weighting for least squares to be 0 for phases those who listed in phase_fine
+      w = if(is.null(phase_fine)) {
+        1
+      } else {
+        as.integer(!(phase %in% phase_fine))
+      }
+    ) %>>%
+    group_by(elint) %>>% # Peform least squares and estimate 99% CI and PI
+    mutate(map_est = predict(lm(map ~ 0 + pkint, weights = w))) %>>%
     mutate(
-      x = round((x - cnd$start[1]) * 1000 / cnd$step[1] + 1),
-      y = round((y - cnd$start[2]) * 1000 / cnd$step[2] + 1)) %>>%
-    filter(x > 0 & y > 0 & x <= cnd$px[1] & y <= cnd$px[2]) %>>% #remove data out of maps
-    mutate(nr = (y-1) * cnd$px[1] + x) %>>%
-    distinct(nr, .keep_all = TRUE)
-
-  #find initial centers
-  centers <- qltmap %>>%
-    lapply(unlist, use.names = FALSE) %>>%
-    lapply(`[`, qnt$cnd$nr) %>>%
-    lapply(as.double) %>>%
-    as.data.table %>>%
-    setcolorder(order(names(.))) %>>%
-    mutate(phase = qnt$cnd$phase) %>>%
-    filter(!is.na(phase)) %>>%
-    arrange(phase) %>>%
-    gather(elm, val, -phase) %>>%
-    group_by(phase, elm) %>>%
-    summarise(val = median(val)) %>>%
+      # map_ci_L99 = qgamma(0.005, map_est + 1, 1),
+      # map_ci_H99 = qgamma(0.995, map_est + 1, 1),
+      map_pi_L99 = qnbinom(0.005, map_est + 1, 0.5),
+      map_pi_H99 = qnbinom(0.995, map_est + 1, 0.5)
+    ) %>>%
+    mutate( # See if values are within CI or PI
+      # within_ci = (map <= map_ci_H99) & (map >= map_ci_L99),
+      within_pi = (map <= map_pi_H99) & (map >= map_pi_L99)
+    ) %>>%
+    group_by(elint, phase) %>>%
+    mutate(n_within_pi = sum(within_pi)) %>>%
+    ungroup() %>>%
+    mutate(map = ifelse(within_pi == 0, map_est, map)) %>>%
+    # filter(within_ci) %>>%
+    filter(within_pi | n_within_pi == 0) %>>%
+    group_by(elint, phase) %>>%
+    summarise(map = median(map)) %>>%
     ungroup %>>%
-    spread(elm, val)
+    spread(elint, map) %>>%
+    (~ if(is.character(saving)) fwrite(., saving)) %>>%
+    # (~ print(.)) %>>%
+    return()
 
-  if(is.character(saving)) fwrite(centers, saving)
-
-  return(centers)
 }
 
 
