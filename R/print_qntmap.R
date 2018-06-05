de4afaf360dfaaa4fec4e7af533056a46464f93b
atusy <30277794+atusy@users.noreply.github.com>
2018-06-05 22:06:44 +0900

    perf less use of cat

--- R/print_qntmap.R
+++ R/print_qntmap.R
@@ -16,13 +16,16 @@ print.qntmap <- function(x, summarizing = TRUE, ...) {
     ' mass concentration map\n'
   ))
   print(summary.qntmap(x))
-  cat('\n')
-  cat('This is a list object\n')
-  cat('x$CaO$wt returns CaO mass concentration map, and\n')
-  cat('x$CaO$se returns CaO standard error map\n')
-  cat('\n')
-  cat('You can also access to the above data in csv files\n')
-  cat('saved in "qntmap" directory below your mapping data directory\n')
-  cat('e.g., example/.map/1/qntmap/CaO_wt.csv')
+  cat(
+    '',
+    'This is a list object',
+    'x$CaO$wt returns CaO mass concentration map, and', 
+    'x$CaO$se returns CaO standard error map', 
+    '', 
+    'You can also access to the above data in csv files', 
+    'saved in "qntmap" directory below your mapping data directory', 
+    'e.g., example/.map/1/qntmap/CaO_wt.csv',
+    sep = '\n'
+  )
   invisible(x)
 }
