6977cc8544a80248382f00211c8ff86f28044951
atusy <30277794+atusy@users.noreply.github.com>
2018-06-06 09:56:09 +0900

    unexported

--- R/flag0.R
+++ R/flag0.R
@@ -1,7 +1,6 @@
 #' return integer as character flagged with 0
 #'
 #' @param x integer
-#' @export
 flag0 <- function(x) {
   formatC(x, width = floor(log10(max(x, na.rm = TRUE))) + 1, flag = '0')
 }
