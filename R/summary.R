40645995789ab329f78c354f5c980d1abb7f8f7b
atusy <30277794+atusy@users.noreply.github.com>
2018-06-22 10:29:43 +0900

    merge summary_*.R to summary.R

---
+++ R/summary.R
@@ -0,0 +1,36 @@
+#' summary qltmap_cls data
+#'
+#' Returns abundance ratios of clusters.
+#'
+#' @param object qntmap class data
+#' @param ... other arguments are discarded
+#'
+#' @importFrom pipeline 
+#'
+#'@export
+#'
+summary.qltmap_cls <- function(object, ...) {pipeline({
+  object$membership 
+  colSums 
+  `/`(sum(.))
+})}
+
+#' summary qntmap class data.
+#'
+#' @param object qntmap class data
+#' @param ... other arguments are discarded
+#'
+#' @importFrom pipeR pipeline
+#' @importFrom dplyr bind_rows
+#'
+#'@export
+summary.qntmap <- function(object, ...) {pipeline({
+  object 
+  lapply(`[[`, 'wt') 
+  lapply(unlist) 
+  lapply(summary) 
+  lapply(round, 2) 
+  lapply(as.list) 
+  bind_rows(.id = 'element') 
+  as.data.frame()
+})}
