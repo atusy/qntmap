10bcf8b4c575073e696f7c03b5fd288d3cc0803e
atusy <30277794+atusy@users.noreply.github.com>
2018-08-16 09:24:07 +0900

    refactor

--- R/qltmap_cls_pois_integrate.R
+++ R/qltmap_cls_pois_integrate.R
@@ -7,22 +7,21 @@
 #' @importFrom stringr str_replace
 #'
 #' @export
-qltmap_cls_pois_integrate <- function(result, wd = NULL, saving = TRUE) {
+qltmap_cls_pois_integrate <- function(result, wd = '.', saving = TRUE) {
   cd <- getwd()
   on.exit(setwd(cd))
+  setwd(wd)
 
-  #####set working directory
-  if(!is.null(wd)) setwd(wd)
-
-  #####integrate clusters on demand
+  # integrate clusters on demand
   cls <- as.factor(str_replace(colnames(result$membership), '_.*$', ''))
 
-  #####modify result
+  # modify result
   result$ytehat <- result$ytehat %>>%
     names %>>%
     str_replace('_.*$', '') %>>%
     as.factor %>>%
-    (setNames(as.numeric(.), as.character(.)))
+    (setNames(as.integer(.), as.character(.)))
+  
   result$membership <- sapply(
     levels(cls),
     function(i) rowSums(result$membership[, cls == i, drop = FALSE])
