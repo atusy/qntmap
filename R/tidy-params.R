26eeca7f8a30467addec22d90ee598a552b305e1
Unknown <30277794+atusy@users.noreply.github.com>
2019-02-05 18:49:38 +0900

    change+test: tidy_params() suppress returning standard errors

--- R/tidy-params.R
+++ R/tidy-params.R
@@ -9,11 +9,8 @@ tidy_params <- function (AG, B, qnt) {
   params <- left_join(AG, B, by = c("elm")) %>>%
     left_join(rename(qnt$elm[c("elem", "elint")], elm = elem), by = "elm") %>>%
     transmute(
-      stage = stg, element = elm, elint, phase = phase3, 
-      alpha = a, alpha_se = a_se,
-      beta = b,  beta_se = b_se, 
-      gamma = g, gamma_se = g_se,
-      wt = NA_real_
+      stage = stg, oxide = elm, element = elint, phase = phase3, 
+      alpha = a, beta = b, gamma = g, wt = NA_real_
     ) 
   if(length(unique(B$stg)) == 1L) params$stage <- NULL
   params
---
+++ tests/testthat/test-tidy-params.R
@@ -0,0 +1,26 @@
+context("tidy_params()")
+
+if(interactive()) setwd(here::here("tests/testthat"))
+
+xmap <- read_xmap("minimal/.map/1", saving = FALSE)
+qnt <- read_qnt("minimal/.qnt", saving = FALSE)
+cluster <- cluster_xmap(xmap, find_centers(xmap, qnt, saveas = FALSE), saving = FALSE)
+epma <- tidy_epma_for_quantify(
+  tidy_epma(qnt, xmap, cluster),
+  maps_x = attr(xmap, "pixel")[1],
+  maps_y = attr(xmap, "pixel")[2],
+  elements = qnt$elm$elem
+)
+
+AG <- find_AG(epma)
+B <- find_B(epma)
+
+test_that("Structure of a returned value by tidy_params()", {
+  params <- tidy_params(AG = AG, B = B, qnt = qnt)
+  expect_s3_class(params, "data.frame")
+  expect_named(params, c("oxide", "element", "phase", "alpha", "beta", "gamma", "wt"))
+  expect_equal(
+    unname(sapply(params, class)), 
+    c("character", "character", "character", "numeric", "numeric", "numeric", "numeric")
+  )
+})
