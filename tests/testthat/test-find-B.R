ee396e73b29cd79dfc7d7b6ba3e92737e2e1c518
atusy <30277794+atusy@users.noreply.github.com>
2019-06-24 22:23:19 +0900

    add+update: test on find-B.R

--- tests/testthat/test-find-B.R
+++ tests/testthat/test-find-B.R
@@ -4,6 +4,11 @@ if (interactive()) setwd(here::here("tests/testthat"))
 
 if (!exists("xmap")) source("setup.R")
 
+test_that(
+  "lm_B() has arguments same as find_outlier except `...` and `remove_outlier`", {
+  expect_identical(formals(lm_B)[-(2:3)], formals(find_outlier)[])
+})
+
 test_that("lm_B(): If `isTRUE(all(mem == 0))`, then coef and vcov return NA", {
   B <- lm_B(mutate(epma2, mem = 0))
   expect_true(all(is.na(B$b)))
@@ -33,24 +38,12 @@ test_that("find_B(): returns a data frame", {
 })
 
 test_that("find_B(): In case map should be divided into smaller maps (e.g., guidenet map)", {
-  B <- find_B(
-    tibble::add_row(
-      epma2,
-      mem = 0,
-      mapint = 0, pkint = 0,
-      stg = "dummy",
-      elm = unique(epma2$elm),
-      dwell = epma2$dwell[1],
-      beam_map = epma2$beam_map[1]
-    )
-  )
+  B <- find_B(dplyr::bind_rows(epma2, dplyr::mutate(epma2, stg = "12")))
+  B_11 <- B[B$stg == "11", ]
+  B_12 <- B[B$stg == "12", ]
   nm <- setdiff(names(B), "stg")
-  B_11 <- B[B$stg == "11", nm]
-  B_11 <- B_11[order(B_11$elm), ]
-  B_dummy <- B[B$stg == "dummy", nm]
-  B_dummy <- B_dummy[order(B_dummy$elm), ]
-  rownames(B_11) <- rownames(B_dummy) <- NULL
-  expect_identical(B_11, B_dummy)
+  rownames(B_12) <- rownames(B_11) <- NULL 
+  expect_identical(B_11[nm], B_12[nm])
 })
 
 if (interactive()) setwd(here::here())
